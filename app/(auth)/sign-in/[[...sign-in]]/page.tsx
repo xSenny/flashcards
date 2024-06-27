2a0fb3c4cc55391815537604fb27e819bdd45188
xSenny <ursuarsenie@gmail.com>
2024-06-27 20:37:22 +0300

    added some seo

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,11 @@
 import { SignIn } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Flashy Learners | Sign Up",
+  description: "Sign in to access Flashy Learners.",
+};
+
 
 export default function Page() {
   return <SignIn />;
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,11 @@
 import { SignUp } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Flashy Learners | Sign Up",
+  description: "Sign up to access Flashy Learners.",
+};
+
 
 export default function Page() {
   return <SignUp />;
